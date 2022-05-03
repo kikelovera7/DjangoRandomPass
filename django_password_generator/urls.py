196d5a40632795acd0345225991c3d38937bb022
Enrique Lovera <kikelovera7@gmail.com>
2022-05-02 23:10:07 -0300

    adding password route, and some logic to generate the random password

--- django_password_generator/urls.py
+++ django_password_generator/urls.py
@@ -19,5 +19,6 @@ from generator import views
 urlpatterns = [
     ##path('admin/', admin.site.urls),
     path('about',views.about),
-    path('',views.home)
+    path('',views.home),
+    path('generate-password',views.password, name='password')
 ]
--- generator/views.py
+++ generator/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from random import choice
 #from django.http import HttpResponse
 
 
@@ -10,4 +11,25 @@ def about(request):
     #return HttpResponse('<h1>hello world from DJANGOOOO</h1>')
     return render(request,'about.html')
 
+def password(request):
+    print(request.GET)
+
+    ###agregar validaciones por las dudas en caso que el usuario tipee un valor directo en la URL
+    characters = list('abcdefghijkmlnñopqrstuvwxyz')
+    lenProperty = int(request.GET.get('length'))
+
+    if 'uppercase' in request.GET:
+        characters += list('ABCDEFGHIJKLMNÑOPQRSTUVWXTZ')
+    
+    if 'numbers' in request.GET:
+        characters += list('1234567890')
+    if 'special' in request.GET:
+        characters += list('/*-+.-.,{}+?¡!"#$%&/()=?¡_\' ')
+    
+
+    generatedPassword=''
+    for character in range(lenProperty):
+        generatedPassword += choice(characters)
+
+    return render(request,'password.html',{'password': generatedPassword})
 
